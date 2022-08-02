fc59a5d5ab50b1c0f75c0556de822bee6411df1f
Marycorreia12 <correia_nety@hotmail.com>
2022-08-02 16:02:35 +0100

    Header component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,12 @@
 import NotificationButton from "./components/NotificationButton"
+import Header from "./components/Header"
 
 function App() {
   return(
+    <>
+    <Header/>
     <NotificationButton/>
+    </>
   )
   
 }
